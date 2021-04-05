fb6939e22bb033b10b85316db4c6b457b14b8de2
mercysu <73023865+mercysu@users.noreply.github.com>
2021-04-05 16:08:15 +0800

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -5,43 +5,63 @@
 #include <ctime>			
 #include <termios.h>		//终端设置
 
-#define MAX_X 20
-#define MAX_Y 30
-bool flag = false;
-bool slow = false;
+#include "MazeMap.h"
+#include "MazeMan.h"
 
 using namespace std;
 
-int maze[MAX_X][MAX_Y];		//迷宫
 
-void menu(){
-	system("clear");
-	int num;
-	cout<<"\t****************************************"<<endl;
-	cout<<"\t*                                      *"<<endl;
-	cout<<"\t*                                      *"<<endl;
-	cout<<"\t*               1.自行游戏             *"<<endl;
-	cout<<"\t*                                      *"<<endl;
-	cout<<"\t*               2.退出游戏             *"<<endl;
-	cout<<"\t*                                      *"<<endl;
-	cout<<"\t****************************************"<<endl;
-	slow = false;
-	switch(getch()){
-		case '1':
-			gamestart();
-			break;
-		case '2':
-			exit(1);break;
-		default:
-			cout<<"\n\n              错误操作，输入enter返回！"<<endl;
-			getchar();
-			menu();
-	}
-	getchar();
-}
+int main()
+{
+  //定义并初始化存储地图数据的二维数组
+#define MapRow 11    //迷宫地图的行数
+#define MapCol 11    //迷宫地图的列数
+int map[MapRow][MapCol];	//迷宫
+ 
 
-//put in another main file
-int main(int argc,char** argv){
-	menu();
-	return 0;
+//创建迷宫
+void createMaze(){
+	int maxway = MapRow * MapCol;		//最大通路
+	int x,y;
+
+	for(x=0;x<MapRow;x++)
+		for(y=0;y<MapCol;y++)
+			map[x][y] = 1;			//先填充迷宫
+
+	srand((unsigned)time(NULL));	//随机函数种子，以时间为参数
+	for(int i=0;i<maxway;i++)		//随机构建迷宫通路
+	{
+		x = rand() % (MapRow-2) + 1;
+		y = rand() % (MapCol-2) + 1;
+		map[x][y] = 0;
+	}	
+
+	map[1][1] = 0; 				//入口
+	map[MapRow-2][MapCol-2] = 0;		//出口
+
+	map[0][1] = 3;
+ 	map[MapRow-1][MapCol-2] = 0;
+};
+
+	
+
+  //从堆中实例化地图对象
+  MazeMap *mm = new MazeMap();
+  mm->setMazeMap(&map[0][0],MapRow,MapCol);
+  mm->setExitPosition(10,1);
+  mm->pintMazeMap();
+  //从堆中实例化游戏角色对象
+  MazeMan *man = new MazeMan();
+  //设置角色起始位置
+  man->setPosition(0,9);
+  //设置游戏要走的地图
+  man->setMap(mm);
+  //用new的方式从堆中实例化的对象，使用完毕之后需要手动释放内存
+  man->start();
+  delete mm;
+  mm = NULL;
+  delete man;
+  man = NULL;
+  std::cout << "闯关成功！" << std::endl;
+  system("pause");
 }
