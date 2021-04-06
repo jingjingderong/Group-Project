7e29c141d025ed9155b89e6cec841e39cbf3ba10
mercysu <73023865+mercysu@users.noreply.github.com>
2021-04-06 16:24:29 +0800

    Update MazeMap.h

--- MazeMap.h
+++ MazeMap.h
@@ -2,7 +2,6 @@
 #define MAZEMAP_H
  
 #include <iostream>
-#include <Windows.h>  //COORD
  
 //在二维数组里，1代表墙，0代表路
 #define WALL 1
@@ -21,7 +20,6 @@ public:
   int** getMap();             //返回地图二维数组指针
   const char m_cRoad;           //常数据成员：路
   void setExitPosition(int x,int y);   //设置迷宫出口
-  COORD m_COORDExitPostion;  //迷宫的出口
 private:
   const char m_cWall;     //常数据成员：墙
   int** m_pMap;        //指向迷宫地图二维数组的二级指针
